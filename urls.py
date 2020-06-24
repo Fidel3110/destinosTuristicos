f0d128db559429260cd136eddf8bb9815271acec
Fidel <64174564+Fidel3110@users.noreply.github.com>
2020-06-23 23:51:22 -0500

    version 2

--- urls.py
+++ urls.py
@@ -15,9 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from proyecto.views import saludo
+from proyecto.views import destinos
 #TUPLA
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('saludo/',saludo),
+    path('destinos/',destinos),
 ]
