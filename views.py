887bdf223422e585bd273da6eef110467b8d2c59
Fidel <64174564+Fidel3110@users.noreply.github.com>
2020-06-23 23:50:39 -0500

    version 2

--- views.py
+++ views.py
@@ -1,4 +1,10 @@
 from django.http import HttpResponse
+from django.template import Template, Context
 #VISTA
-def saludo(request):
-    return HttpResponse("Probando..")
+def destinos(request):
+    doc_externo=open("C:/Users/admin/Desktop/pweb/test/proyecto/proyecto/plantillas/home.html")
+    plt=Template(doc_externo.read())
+    doc_externo.close()
+    ctx=Context()
+    documento=plt.render(ctx)
+    return HttpResponse(documento)
