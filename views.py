0f39f9a484b5e8752a4668afab4806a5ccedcd02
Fidel <64174564+Fidel3110@users.noreply.github.com>
2020-06-24 20:10:24 -0500

    version 4

--- views.py
+++ views.py
@@ -1,10 +1,5 @@
+from django.shortcuts import render
 from django.http import HttpResponse
-from django.template import Template, Context
-#VISTA
-def destinos(request):
-    doc_externo=open("C:/Users/admin/Desktop/pweb/test/proyecto/proyecto/plantillas/home.html")
-    plt=Template(doc_externo.read())
-    doc_externo.close()
-    ctx=Context()
-    documento=plt.render(ctx)
-    return HttpResponse(documento)
+# Create your views here.
+def home(request):
+    return HttpResponse("Hello world")
