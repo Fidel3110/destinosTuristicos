6aeba599ff669a55ab9d5c0a4202f20b09790220
Fidel <64174564+Fidel3110@users.noreply.github.com>
2020-06-30 16:15:46 -0500

    version 3

--- views.py
+++ views.py
@@ -1,5 +1,9 @@
 from django.shortcuts import render
-
+from .models import Destination
 # Create your views here.
 def index(request):
-    return render(request,'index.html')
+    dest1= Destination()
+    dest1.name ='Munbai'
+    dest1.desc = 'The City that Never Sleeps'
+    dest1.price=760
+    return render(request,'index.html',{'dest1':dest1})
